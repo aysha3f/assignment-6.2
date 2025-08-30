705d67acc33195cebaef3bbb9366aad8cd7fb1a5
aysha3f <tahiaysha220@gmail.com>
2025-08-30 21:46:34 +0600

    categories

--- js/index.js
+++ js/index.js
@@ -10,3 +10,28 @@ const navLinks = document.getElementById("navLinks");
  document.getElementById("viewMoreBtn").addEventListener("click", () => {
   document.getElementById("adopt").scrollIntoView({ behavior: "smooth" });
 });
+
+// fetch categories from API
+async function loadCategories(){
+  const res = await fetch("https://openapi.programming-hero.com/api/peddy/categories");
+  const data = await res.json();
+  disPlayCategories(data.categories);
+};
+// show categories as buttons
+function disPlayCategories (categories){
+  const categoryContainer = document.getElementById("categories");
+  categoryContainer.innerHTML = "";
+  categories.forEach(item => {
+    const button = document.createElement("button");
+   button.innerHTML = `
+  <img src="${item.category_icon}" alt="${item.category}" class="w-6 h-6 inline-block mr-2">
+      ${item.category}
+    `;
+    button.className = "px-4 py-2  rounded-lg ";
+button.addEventListener("click", () => loadPetsByCategory(item.category));
+ categoryContainer.appendChild(button);
+  });
+};
+loadCategories();
+
+
